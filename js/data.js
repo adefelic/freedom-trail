59ae60a421aca0d1dcc0e4b0fd5ba9dcc514ba6b
defelice <ad.defelice@live.com>
2016-04-04 14:47:21 -0400

    added health and food, fail state now based on health

--- js/data.js
+++ js/data.js
@@ -14,14 +14,14 @@ var encounters = [
 			{
 				text: 'Look at the Bunker Hill Monument',
 				resulttext: 'sure is big',
-				isFail: false,
-				nextEncounter: null
+				health: 0,
+				food: 0
 			},
 			{
 				text: 'Ignore the Bunker Hill Monument and move on',
 				resulttext: 'life is too short for phallic memorials',
-				isFail: false,
-				nextEncounter: null
+				health: 0,
+				food: 0
 			},
 		]
 	},
@@ -32,17 +32,20 @@ var encounters = [
 			{
 				text: 'admire the statue',
 				resulttext: 'the attention to detail is striking',
-				isFail: false,
+				health: 0,
+				food: 0
 			},
 			{
 				text: 'admire the greenery',
 				resulttext: 'smells like it was just cut, mmm',
-				isFail: false,
+				health: 0,
+				food: 0
 			},
 			{
 				text: 'go home',
 				resulttext: 'so long! we hope you enjoyed the sights and perils of the FREEDOM TRAIL!',
-				isFail: false,
+				health: 0,
+				food: 0
 			},
 		]
 	},
@@ -52,18 +55,23 @@ var encounters = [
 		options: [
 			{
 				text: 'fight the bear',
-				resulttext: 'rip you die',
-				isFail: true,
+				resulttext: 'didn\'t work!',
+				health: -1,
+				food: 0,
+				causeofdeath: 'bear'
 			},
 			{
 				text: 'run away',
 				resulttext: 'the bear was too fast',
-				isFail: true,
+				health: -1,
+				food: 0,
+				causeofdeath: 'bear'
 			},
 			{
 				text: 'close your eyes',
 				resulttext: 'it worked!',
-				isFail: false,
+				health: 0,
+				food: 0
 			},
 		]
 	},
@@ -74,17 +82,22 @@ var encounters = [
 			{
 				text: 'pet the cat',
 				resulttext: 'purr purr purr',
-				isFail: false,
+				health: 0,
+				food: 0
 			},
 			{
 				text: 'steal the money',
 				resulttext: 'claws so sharp',
-				isFail: true,
+				health: -1,
+				food: 0,
+				causeofdeath: 'cat'
 			},
 			{
 				text: 'flaunt your own wealth',
-				resulttext: 'now the cat is jealous',
-				isFail: true,
+				resulttext: 'now the cat is jealous and took some food',
+				health: 0,
+				food: -1,
+				causeofdeath: 'starvation'
 			},
 		]
 	}
@@ -97,3 +110,19 @@ var wordsForNext = [
 	'anyways,',
 	'moving on,'
 ];
+
+var health = [
+	'dead',
+	'very poor',
+	'poor',
+	'fair',
+	'good'
+];
+
+var screen = {
+	ENCOUNTER: 0,
+	RESULTS: 1,
+	FAIL: 2,
+	START: 3
+};
+
--- js/game.js
+++ js/game.js
@@ -1,73 +1,34 @@
+
+/*
+ * THE FLOW:
+ *	do one of these three things:
+ *	encounter() -> advance() -> results() or fail()
+ *  results()   -> advance() -> encounter() or fail()
+ *	fail()      -> advance() -> encounter()
+ */
+
+ /*
+you can fail through: 
+- having an event decrement your health (die on results screen)
+- losing health to upkeep (die on encounter page)
+
+ */
+
 window.onload = function() {
 
-	var Game = function() {};
-	// the id of the current encounter
-	Game.prototype.encounter = 0;
-	Game.prototype.encounters = 0;
-	Game.prototype.inEncounter = true; // encounter page or results page?
-	// the player's score
-	Game.prototype.score = 0;
-	// is the game over?
-	Game.prototype.gameOver = false;
-
-	function score(i) {
-		if ( ! encounters[state.encounter].options[i].isFail ) {
-			state.score += 10;
-		} else {
-			state.gameOver = true;
-		}
-	}
-	function displayScore() {
-		$('#score').text(state.score);
-	}
-
-	// display encounter information
-	function displayEncounter() {
-		clearText();
-		// get the encounter
-		var enc = encounters[state.encounter];
-
-		// display the encounter image
-		$('#event-img').css('background-image', 'url(img/' + enc.img + ')');
-
-		// display the encounter text
-		$('#event-text').text(enc.text);
-
-		// give them options some text
-		// disable unused buttons while we're in there
-		var optionCount = enc.options.length;
-		$('#options > a').each(function(i,el){
-			if (i < optionCount) {
-				$(el).removeClass('disable');
-				$(el).children( 'p' ).text(enc.options[i].text);
-			}
-		});
-	}
-
-	// display the results of the encounter
-	function displayResults(i) {
-		clearText();
-		// display the result of the player's decision
-		var enc = encounters[state.encounter];
-		$('#event-text').text(enc.options[i].resulttext);
-
-		if (state.gameOver) {
-			// go to fail screen, offer reset.
-			$('#option-0').removeClass('disable');
-			$('#option-0').children( 'p' ).text('you die');
-			state.score = 0;
-			displayScore();
-			state.gameOver = false;
-		} else {
-			// let the player click through to the next encounter
-			$('#option-0').removeClass('disable');
-			$('#option-0').children( 'p' ).text(wordsForNext[randomVerb()]);
-		}
-	}
+	var Game = function() {
+		this.encounter = 0; // id of active encounter
+		this.encounters = 0; // total # of encounters succeeded this game
+		this.screen = screen.START; // type of page we're on
+		this.score = 0; // the player's score
+		this.food = 10; // player food
+		this.health = 4; // player health, die at -1
+	};
 
 	// clears UI text
 	function clearText() {
 		$('#event-text').text('');
+		$('#rip').text('');
 		$('#options > a').each(function(i,el){
 			$(el).addClass('disable');
 			$(el).children( 'p' ).text('');
@@ -86,7 +47,7 @@ window.onload = function() {
 	// get the next encounter; either a critical path encounter or a random one.
 	function getNextEncounter() {
 		switch (++state.encounters) {
-			case 5: // uss constitution
+			case 5: // state house
 				state.encounter = 1;
 				break;
 			default:
@@ -95,26 +56,142 @@ window.onload = function() {
 		}
 	}
 
-	// the player clicks: go to an encounter or a results page
-	function advance(i) {
-		if (state.gameOver === true) {
-			state = new Game();
+	function isFail() {
+		if (state.health < 1) {
+			console.log( 'fail' );
+			return true;
+		}
+		return false;
+	}
+
+	// decrement player food, if no food, worsen health instead
+	function upkeep() {
+		if (state.food > 0) {
+			state.food--;
+		} else {
+			state.health--;
+		}
+	}
+
+	function updateScore(i) {state.score += 10;}
+
+	function displayStatus() {
+		$('#score').text(state.score);
+		$('#food').text(state.food);
+		$('#health').text(health[state.health]);
+	}
+
+	// encounter logic
+	function encounter() {
+		state.screen = screen.ENCOUNTER;
+		// get the encounter
+		var enc = encounters[state.encounter];
+		// display encounter image
+		$('#event-img').css('background-image', 'url(img/' + enc.img + ')');
+		// display encounter text
+		$('#event-text').text(enc.text);
+		// display encounter options; disable unused buttons
+		var optionCount = enc.options.length;
+		$('#options > a').each(function(i,el){
+			if (i < optionCount) {
+				$(el).removeClass('disable');
+				$(el).children( 'p' ).text(enc.options[i].text);
+			}
+		});
+	}
+
+	// results logic
+	function results(i) {
+		state.screen = screen.RESULTS;
+		// get the current encounter
+		var enc = encounters[state.encounter];
+
+		// update the score
+		updateScore(i);
+		// update food and health
+		state.food += enc.options[i].food;
+		state.health += enc.options[i].health;
+		// check if the encounter caused a failure
+		if (isFail()) {
+			// display fail
+			fail(enc.options[i].causeofdeath, i);
 			return;
 		}
-		switch (state.inEncounter) {
-			case true:
-				state.inEncounter = false;
-				score(i);
-				displayScore();
-				displayResults(i);
+		displayStatus();
+
+		// display results text
+		$('#event-text').text(enc.options[i].resulttext);
+		// let the player click through to the next encounter
+		$('#option-0').removeClass('disable');
+		$('#option-0').children( 'p' ).text(wordsForNext[randomVerb()]);
+	}
+
+	// fail logic
+	function fail(causeofdeath, i) {
+		state.screen = screen.FAIL;
+		var resultText = '';
+		if (i !== null) {
+			resultText = encounters[state.encounter].options[i].resulttext;
+		}
+		// if there's an event result, display it
+		$('#event-text').text(resultText);
+		// display cause of death
+		$('#rip').text('you died of ' + causeofdeath);
+		// offer game reset
+		$('#option-0').removeClass('disable');
+		$('#option-0').children( 'p' ).text('try again?');
+	}
+
+	// this fires whenever the player clicks a button
+	// i is the index of the option clicked
+	function advance(i) {
+		
+		// reset text on screen
+		clearText();
+
+		if (state.screen !== screen.FAIL) {
+			// upkeep (decrement food or health)
+			upkeep();
+			displayStatus();
+			if (isFail()) {
+				// if the chosen option would've killed the player, make that happen instead
+				// this is dumb and bad programming
+				var causeOfDeath = encounters[state.encounter].options[i].causeofdeath;
+				if (typeof causeOfDeath != "undefined") {
+					fail(causeOfDeath, i);
+				} else {
+					fail('starvation', null);
+				}
+				return;
+			}
+		}
+
+		// decide our next screen
+		switch (state.screen) {
+
+			// clicked from fail ("try again")
+			case screen.FAIL:
+				state = new Game();
+				displayStatus();
+				encounter();
 				break;
-			case false:
-				state.inEncounter = true;
+
+			// clicked from an encounter
+			case screen.ENCOUNTER:
+				// display results
+				results(i);
+				break;
+
+			// clicked from the start page
+			case screen.START:
+
+			// clicked from a results page
+			case screen.RESULTS:
+				// get new encounter
 				getNextEncounter();
-				displayEncounter();
+				// display new encounter
+				encounter();
 				break;
-			default:
-				// ???
 		}
 	}
 
@@ -128,9 +205,10 @@ window.onload = function() {
 	function main() {
 		initClickHandlers();
 		// display the first encounter
-		displayEncounter();
+		encounter();
 	}
 
 	var state = new Game();
+	displayStatus();
 	main();
 };
